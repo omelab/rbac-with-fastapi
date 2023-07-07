50b6c84dfdb80bbb7fb1f1888faa63e06b13e2f5
Abu Bakar <a.bakar87@gmail.com>
2023-07-07 13:05:42 +0600

    lets create fast api

--- main.py
+++ main.py
@@ -13,8 +13,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.get("/about")
-def about():
-    return {"page": "About"}
