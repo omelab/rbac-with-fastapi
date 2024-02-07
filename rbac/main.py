df1d5824082a88d881695753ca0f49cf82096349
Abu Bakar <a.bakar87@gmail.com>
2024-02-07 22:44:52 +0600

    change done

--- rbac/main.py
+++ rbac/main.py
@@ -1,14 +1,28 @@
 from typing import Union
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title = "RBAC API",
+    description = "This is simple RBAC API for best practice development",
+    docs_url = "/",
+)
 
-@app.get("/")
-async def read_root():
+@app.get("/notes")
+async def get_all_notes():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/notes")
+async def create_notes():
+    pass
 
+@app.get("/notes/{note_id}")
+async def get_note_by_id(note_id: int, q: Union[str, None] = None):
+    return {"item_id": note_id, "q": q}
 
+@app.put("/notes/{note_id}")
+async def update_note():
+    pass
+
+@app.delete("/notes/{note_id}")
+async def delete_note():
+    pass
